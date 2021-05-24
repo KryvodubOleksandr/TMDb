3683b0799f585e2c159de26f806d95bac8e1f3a9
Alexander Sharko <krivodubag@gmal.com>
2021-05-24 12:56:20 +0300

    Initial Commit

---
+++ TMDb/TMDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDbApp.swift
+//  TMDb
+//
+//  Created by Alexander Sharko on 24.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct TMDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
